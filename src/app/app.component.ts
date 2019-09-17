89ce7c6abce10928c38fc0b6212780bceb078a2a
vdias38 <victor.dias@meumobi.com>
2019-09-17 15:12:05 +0100

    firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Item, ItemService } from '@meumobi/nfmb-data-mgmt';
 
 @Component({
   selector: 'nfmb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nfmb-tv';
+export class AppComponent implements OnInit {
+  items$: Observable<Item[]>;
+
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit() {
+    this.items$ = this.itemService.data$;
+  }
 }
