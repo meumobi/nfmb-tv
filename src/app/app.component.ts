e55a06a5ca7b6c0af9a9988473ab73483ffb56c7
vdias38 <victor.dias@meumobi.com>
2019-09-17 02:37:18 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nfmb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'nfmb-tv';
+}
