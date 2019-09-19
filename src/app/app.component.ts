785d4bee949daab338db7b2fb12c65c08589d77d
vdias38 <victor.dias@meumobi.com>
2019-09-19 14:16:18 +0100

    add google-startup template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,29 @@ import { Item, ItemService } from '@meumobi/nfmb-data-mgmt';
 })
 export class AppComponent implements OnInit {
   items$: Observable<Item[]>;
+  currentSlide = 0;
+  sliderLength: number;
 
   constructor(private itemService: ItemService) { }
 
   ngOnInit() {
     this.items$ = this.itemService.data$;
+
+    this.items$.subscribe(data => {
+      this.sliderLength = data.length;
+      console.log('Slides number: ', this.sliderLength);
+    });
+
+    setInterval(() => {
+      this.updateCurrentSlide();
+    }, 2000);
+  }
+
+  private updateCurrentSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.sliderLength;
+  }
+
+  public isActive(index: number) {
+    return this.currentSlide === index;
   }
 }
